b4cf23905416406967fca570b880e14a1db3be1f
Julieth <juliethquinchia@gmail.com>
2020-04-19 21:17:06 -0500

    calculadora

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -112,40 +112,45 @@ class Calculadora extends React.Component {
 
     render() {
         return (
-            <div className="card container justify-content-center contenedor">
-                <div className="card container mx-0 ml-5 pantalla">
-                    <h4 className="text-white">{this.state.resultado}</h4>
-                    <div className="card pantalla">  <h4 className="text-white">{this.state.letras} </h4> </div>
+            <div className="card container justify-content-center contenedor ">
+                <div className="card container  pantalla letras mt-2">
+                    <h4 className="text-black">{this.state.resultado}</h4>
+                    <div>  <h4 className="text-black">{this.state.letras} </h4> </div>
                 </div>
-                <div className="container mx-0 ml-5">
+                <div className="container mx-0 ml-3 mt-3">
                     <div className="btn-toolbar " role="toolbar" aria-label="Toolbar with button groups">
                         <div class="btn-group mr-2" role="group" aria-label="First group">
 
-                            <button type="button" class="btn btn-secondary" onClick={this.Cero} > {this.state.cero} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Uno} > {this.state.uno} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Dos}> {this.state.dos} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Tres}> {this.state.tres} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Punto}> . </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Cero} > {this.state.cero} </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Uno} > {this.state.uno} </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Dos}> {this.state.dos} </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Tres}> {this.state.tres} </button>
+                            <button type="button" class="btn btn  btn-light mx-1 my-1  " width="150" onClick={this.multiplicar}> * </button>
+                            
+                            <button type="button" class="btn btn-light mx-1 my-1 " onClick={this.sumar}> + </button>
                         </div>
                     </div>
                     <div class="btn-toolbar  " role="toolbar" aria-label="Toolbar with button groups">
                         <div class="btn-group" role="group" aria-label="First group">
-                            <button type="button" class="btn btn-secondary" onClick={this.Cuatro}> {this.state.cuatro} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Cinco}> {this.state.cinco} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Seis}> {this.state.seis} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Siete}> {this.state.siete} </button>
-                            <button type="button" class="btn btn-secondary  mx-0.1 pl-4" onClick={this.restar}>- </button>
-                            <button type="button" class="btn btn-secondary  mr-2 pl-3" onClick={this.sumar}> + </button>
+                            <button type="button" class="btn  btn-light mx-1 my-1" onClick={this.Cuatro}> {this.state.cuatro} </button>
+                            <button type="button" class="btn  btn-light mx-1 my-1" onClick={this.Cinco}> {this.state.cinco} </button>
+                            <button type="button" class="btn  btn-light mx-1 my-1 " onClick={this.Seis}> {this.state.seis} </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Siete}> {this.state.siete} </button>
+                            <button type="button" class="btn  btn-light mx-1 my-1  " onClick={this.restar}>- </button>
+                            <button type="button" class="btn btn  btn-light mx-1 my-1" onClick={this.dividir}> /  </button>
+    
                         </div>
                     </div>
                     <div class="btn-toolbar " role="toolbar" aria-label="Toolbar with button groups">
                         <div class="btn-group" role="group" aria-label="First group">
-                            <button type="button" class="btn btn-secondary" onClick={this.Ocho}> {this.state.ocho} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Nueve}> {this.state.nueve} </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.cleaner} > AC </button>
-                            <button type="button" class="btn btn-secondary" onClick={this.Igual}> = </button>
-                            <button type="button" class="btn btn-secondary  " width="150" onClick={this.multiplicar}> * </button>
-                            <button type="button" class="btn btn-secondary pr-3" onClick={this.dividir}> /  </button>
+                            <button type="button" class="btn btn  btn-light mx-1 my-1" onClick={this.Ocho}> {this.state.ocho} </button>
+                            <button type="button" class="btn btn  btn-light mx-1 my-1" onClick={this.Nueve}> {this.state.nueve} </button>
+                            <button type="button" class="btn btn-light mx-1 my-1" onClick={this.Punto}> . </button>
+                            <button type="button" class="btn btn  btn-light mx-2 my-1" onClick={this.Igual}> = </button>
+                            <button type="button" class="btn btn  btn-light mx-1 my-1 btn-block" onClick={this.cleaner} > AC </button>
+             
+        
+                            
                         </div>
                     </div>
                 </div>
