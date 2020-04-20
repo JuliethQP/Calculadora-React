fd30d8beefe9de12d0b7d1f6e1f584dde6c10a2f
Julieth <juliethquinchia@gmail.com>
2020-04-19 20:48:14 -0500

    Updates

---
+++ precache-manifest.a2a1849a21fb1e73b94607ec0fc51035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbaa6922295aa86a4ac20c8b946089f",
+    "url": "/Calculadora-React/index.html"
+  },
+  {
+    "revision": "f708608517f51c402807",
+    "url": "/Calculadora-React/static/css/main.97e26ece.chunk.css"
+  },
+  {
+    "revision": "3fb989361ea6d5787e32",
+    "url": "/Calculadora-React/static/js/2.68a66a8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculadora-React/static/js/2.68a66a8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f708608517f51c402807",
+    "url": "/Calculadora-React/static/js/main.45d7a559.chunk.js"
+  },
+  {
+    "revision": "6c41e6210e04e30ad9a6",
+    "url": "/Calculadora-React/static/js/runtime-main.f49dd975.js"
+  }
+]);
